44e4e98b91779ceba5fb92751041052321b8321c
osha7 <ogroetz@gmail.com>
2020-07-27 17:55:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+
+// function selectDifferentDrivers(arrayofdrivers, func) {
+//     return func(arrayofdrivers)
+// }
+
+// NOT MY CODE: 
+
+function map(sourceArray, cbFunction) {
+    // return setOfFunctions.map(sourceArray)
+    return sourceArray.map(cbFunction)
+}
+
+function reduce(sourceArray, cbFunction, init = 0) {
+    if (init === 0) {
+        return sourceArray.reduce(cbFunction)
+    } else {
+        return sourceArray.reduce(cbFunction, init) 
+    }
+}
